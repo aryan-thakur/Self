2839c82cfa1f1cb224982e43ae24f949b6f4e164
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-03 17:08:48 +0530

    angularjs routing

--- js/AngularJS/app.js
+++ js/AngularJS/app.js
@@ -1,15 +1,15 @@
 var app = angular.module("myApp", ["ngRoute"]);
-/*app.config(function ($routeProvider) {
+app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      controller: 'resultsController',
+    //  controller: 'resultsController',
       templateUrl: './home.html'
     })
-    .when('/model/:id', {
-     controller: 'pageController',
-     templateUrl: './model_page.html?v=23'
-    })
+  //  .when('/model/:id', {
+  //   controller: 'pageController',
+  //   templateUrl: './model_page.html?v=23'
+  //  })
     .otherwise({
       redirectTo: '/'
     });
-});*/
+});
