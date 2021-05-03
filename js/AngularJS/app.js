b2d1ad87513eef1fca3653322fd9f869677af692
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-03 16:54:42 +0530

    initial files

---
+++ js/AngularJS/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("myApp", ["ngRoute"]);
+/*app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: 'resultsController',
+      templateUrl: './home.html'
+    })
+    .when('/model/:id', {
+     controller: 'pageController',
+     templateUrl: './model_page.html?v=23'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});*/
