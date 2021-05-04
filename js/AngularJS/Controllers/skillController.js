c2d2c0fdd97f30b4526e27302c84a962b54e4d5a
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-04 17:10:54 +0530

    syntax error fixed

--- js/AngularJS/Controllers/skillController.js
+++ js/AngularJS/Controllers/skillController.js
@@ -1,6 +1,6 @@
 app.controller('skillController', function($scope, $http) {
 $scope.skills = [
-  {id: "foo", title: "Python" value: "20"},
-  {id: "foo", title: "JAVA" value: "90"}
+  {id: "foo", title: "Python", value: "20"},
+  {id: "foo", title: "JAVA", value: "90"}
 ];
 })
