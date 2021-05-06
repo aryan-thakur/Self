4cd4451e697bd7a3418bdfdf769fbc584fd44b9a
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-06 12:39:35 +0530

    skills page link added to index, css fixeed

--- js/AngularJS/Controllers/indexController.js
+++ js/AngularJS/Controllers/indexController.js
@@ -9,24 +9,28 @@ app.controller('indexController', function($scope, $http) {
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
+      location.href = "";
     }
     else if(value == 1){
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#a9a9a9"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
+      location.href = "#!skills";
     }
     else if(value == 2){
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#a9a9a9"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
+      location.href = "#!projects";
     }
     else{
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#a9a9a9"};
+      location.href = "#!newsboard";
     }
   }
 });
