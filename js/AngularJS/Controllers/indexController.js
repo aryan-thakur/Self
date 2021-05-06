415a5e0e3b26b097b57e9c0317804d56e47bd683
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-06 15:50:17 +0530

    final fixes skills

--- js/AngularJS/Controllers/indexController.js
+++ js/AngularJS/Controllers/indexController.js
@@ -1,36 +1,51 @@
-app.controller('indexController', function($scope, $http) {
-  $scope.homeStyle = {'background-color': "#a9a9a9"};
-  $scope.skillsStyle = {'background-color': "#5c5470"};
-  $scope.projectsStyle = {'background-color': "#5c5470"};
-  $scope.newsboardStyle = {'background-color': "#5c5470"};
-  $scope.navigate = function(value){
-    if(value == 0){
+app.controller('indexController', function($scope, $http, $route) {
+
+  $scope.initfn = function(){
+    var chapter = location.href.split('/')[location.href.split('/').length - 1];
+    var value = 0;
+    if(chapter == "skills"){
+      value = 1;
+    }
+    else if(chapter == "projects"){
+      value = 2;
+    }
+    else if(chapter == "newsboard"){
+      value = 3;
+    }
+    $scope.navigate(value,0);
+  }
+  $scope.navigate = function(redirect, val){
+    if(redirect == 0){
       $scope.homeStyle = {'background-color': "#a9a9a9"};
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
-      location.href = "";
+      if(val){
+      location.href = "#!";}
     }
-    else if(value == 1){
+    else if(redirect == 1){
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#a9a9a9"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
-      location.href = "#!skills";
+        if(val){
+      location.href = "#!skills";}
     }
-    else if(value == 2){
+    else if(redirect == 2){
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#a9a9a9"};
       $scope.newsboardStyle = {'background-color': "#5c5470"};
-      location.href = "#!projects";
+      if(val){
+      location.href = "#!projects";}
     }
     else{
       $scope.homeStyle = {'background-color': "#5c5470"};
       $scope.skillsStyle = {'background-color': "#5c5470"};
       $scope.projectsStyle = {'background-color': "#5c5470"};
       $scope.newsboardStyle = {'background-color': "#a9a9a9"};
-      location.href = "#!newsboard";
+        if(val){
+      location.href = "#!newsboard";}
     }
   }
 });
