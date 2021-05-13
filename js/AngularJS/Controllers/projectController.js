9f152995fdec68a3ac53be48ecc49f5f47d214bf
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-13 13:28:10 +0530

    icon changes

--- js/AngularJS/Controllers/projectController.js
+++ js/AngularJS/Controllers/projectController.js
@@ -1,6 +1,6 @@
 app.controller('projectController', function($scope, $http) {
 $scope.scratchprojects = [
-  {name: "Modelo", src: ["./Images/angularjs_icon.png", "./Images/php_icon.svg", "./Images/css_icon.svg", "./Images/html_icon.svg"], solo: "solo"},
+  {name: "Modelo", src: ["./Images/angularjs_icon.png", "./Images/php_icon.svg", "./Images/css_icon.svg", "./Images/javascript_icon.svg"], solo: "solo"},
   {name: "MIPS Game", src: ["./Images/assembly_icon.svg", "", "", ""], solo: "solo"},
   {name: "Social Media for Seniors", src: ["./Images/justinmind_icon.png", "", "./Images/balsamiq_icon.png", ""], solo: "group"},
   {name: "Linux Shell", src: ["./Images/java_icon.svg", "", "", ""], solo: "group"}
@@ -8,6 +8,6 @@ $scope.scratchprojects = [
 
 $scope.existingprojects = [
   {name: "Pintos", src: ["./Images/c_icon.svg", "", "./Images/docker_icon.svg", ""], solo: "group"},
-  {name: "BlueBoard", src: ["./Images/angularjs_icon.png", "./Images/javascript_icon.svg", "./Images/html_icon.svg", "./Images/css_icon.svg"], solo: "group"}
+  {name: "BlueBoard", src: ["./Images/angularjs_icon.png", "./Images/typescript_icon.svg", "./Images/html_icon.svg", "./Images/sass_icon.svg"], solo: "group"}
 ];
 })
