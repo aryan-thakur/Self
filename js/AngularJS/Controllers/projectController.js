ed61f25939b692628c8a13a2068be70c44b878ed
Aryan Thakur <aryanthakur@Aryan.local>
2021-05-13 13:35:35 +0530

    icon changes

--- js/AngularJS/Controllers/projectController.js
+++ js/AngularJS/Controllers/projectController.js
@@ -2,7 +2,7 @@ app.controller('projectController', function($scope, $http) {
 $scope.scratchprojects = [
   {name: "Modelo", src: ["./Images/angularjs_icon.png", "./Images/php_icon.svg", "./Images/css_icon.svg", "./Images/javascript_icon.svg"], solo: "solo"},
   {name: "MIPS Game", src: ["./Images/assembly_icon.svg", "", "", ""], solo: "solo"},
-  {name: "Social Media for Seniors", src: ["./Images/justinmind_icon.png", "", "./Images/balsamiq_icon.png", ""], solo: "group"},
+  {name: "Social Media for Seniors", src: ["./Images/justinmind_icon.svg", "", "./Images/balsamiq_icon.png", ""], solo: "group"},
   {name: "Linux Shell", src: ["./Images/java_icon.svg", "", "", ""], solo: "group"}
 ];
 
